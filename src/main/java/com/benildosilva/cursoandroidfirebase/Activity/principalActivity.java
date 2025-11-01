50c4bb99ad2e5a73a5073cf17433363fe5213919
Benildo <benalopesdasilva10@gmail.com>
2025-11-01 19:05:34 -0300

    mudando o tipo de usuario prioridade

--- src/main/java/com/benildosilva/cursoandroidfirebase/Activity/principalActivity.java
+++ src/main/java/com/benildosilva/cursoandroidfirebase/Activity/principalActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -12,24 +13,73 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.benildosilva.cursoandroidfirebase.Classes.Usuario;
 import com.benildosilva.cursoandroidfirebase.R;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.security.cert.PolicyNode;
 
 public class principalActivity extends AppCompatActivity {
 
     private FirebaseAuth autenticacao;
+    private DatabaseReference referenciaFirebase;
+    private TextView txtTipoUsuario;
+    private Usuario usuario;
+    private String tipoUsuarioEmail;
+    private Menu menu1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_principal);
+        txtTipoUsuario = (TextView)findViewById(R.id.txtTipoUsuario);
         autenticacao = FirebaseAuth.getInstance();
+        referenciaFirebase = FirebaseDatabase.getInstance().getReference();
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_admin,menu);
+        menu.clear();
+        this.menu1 = menu;
+        String email = autenticacao.getCurrentUser().getEmail().toString();
+
+        referenciaFirebase.child("usuarios").orderByChild("email")
+                .equalTo(email.toString())
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange( DataSnapshot snapshot) {
+                        for (DataSnapshot postSnapshot : snapshot.getChildren()){
+                            tipoUsuarioEmail = postSnapshot.child("tipoUsuario").getValue().toString();
+
+                            txtTipoUsuario.setText(tipoUsuarioEmail);
+
+                            //menu1.clear();
+                            if (tipoUsuarioEmail.equals("Administrador")){
+                                getMenuInflater().inflate(R.menu.menu_admin,menu);
+
+                            } else if (tipoUsuarioEmail.equals("Atendente")) {
+                                getMenuInflater().inflate(R.menu.menu_atend,menu1);
+
+
+                            }
+
+                        }
+
+                    }
+
+                    @Override
+                    public void onCancelled( DatabaseError error) {
+
+                    }
+                });
+
         return true;
     }
 
@@ -42,6 +92,9 @@ public class principalActivity extends AppCompatActivity {
             abrirTelaCadUsuario();
 
         }else if (id == R.id.action_sair_admin){
+            delogarUsuario();
+
+        }else if (id == R.id.action_sair_atend){
             delogarUsuario();
 
         }
