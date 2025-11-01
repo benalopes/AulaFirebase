91ab50084668b39e8b971ecbe05f7a7f8405d1cb
Benildo <benalopesdasilva10@gmail.com>
2025-11-01 11:45:15 -0300

    Ciação pacote Classe

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ android {
 
     defaultConfig {
         applicationId = "com.benildosilva.cursoandroidfirebase"
-        minSdk = 16
+        minSdk = 23
         targetSdk = 36
         versionCode = 1
         versionName = "1.0"
@@ -46,6 +46,7 @@ dependencies {
     androidTestImplementation(libs.espresso.core)
     implementation(platform(libs.firebase.bom))
     implementation("com.google.firebase:firebase-analytics")
+    implementation("com.google.firebase:firebase-auth")
 
 
 }
