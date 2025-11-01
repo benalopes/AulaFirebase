02b3d46c7d8ef9cb2690736104cdf74ab14c9c94
Benildo <benalopesdasilva10@gmail.com>
2025-11-01 15:22:34 -0300

    criando Activity Principal

--- src/main/java/com/benildosilva/cursoandroidfirebase/Activity/MainActivity.java
+++ src/main/java/com/benildosilva/cursoandroidfirebase/Activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.benildosilva.cursoandroidfirebase.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -7,14 +8,17 @@ import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 import com.benildosilva.cursoandroidfirebase.Classes.Usuario;
+import com.benildosilva.cursoandroidfirebase.Dao.configuracaoFirebase;
 import com.benildosilva.cursoandroidfirebase.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -35,24 +39,60 @@ public class MainActivity extends AppCompatActivity {
         edtSenhaLogin = (EditText) findViewById(R.id.edtSenha);
         btnLogin = (Button) findViewById(R.id.btnLogin);
 
+        if (UsuarioLogado()) {
+            Intent intentMinhaConta = new Intent(MainActivity.this,principalActivity.class);
+            abrirNovaActivity(intentMinhaConta);
+        } else {
+            btnLogin.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (!edtEmailLogin.getText().toString().equals("") && (!edtSenhaLogin.getText().toString().equals(""))) {
+                        usuario = new Usuario();
+                        usuario.setEmail(edtEmailLogin.getText().toString());
+                        usuario.setSenha(edtSenhaLogin.getText().toString());
 
-        btnLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!edtEmailLogin.getText().toString().equals("")&& (!edtSenhaLogin.getText().toString().equals(""))){
-                    usuario = new Usuario();
-                    usuario.setEmail(edtEmailLogin.getText().toString());
-                    usuario.setSenha(edtSenhaLogin.getText().toString());
-
-                    validarLogin();
-                }else {
-                    Toast.makeText(MainActivity.this, "Preencha todos os campos", Toast.LENGTH_SHORT).show();
+                        validarLogin();
+                    } else {
+                        Toast.makeText(MainActivity.this, "Preencha todos os campos", Toast.LENGTH_SHORT).show();
+                    }
                 }
-            }
-        });
+            });
 
+        }
     }
     private void validarLogin(){
+        autenticacao = configuracaoFirebase.getFirebaseAuth();
+        autenticacao.signInWithEmailAndPassword(usuario.getEmail().toString(),usuario.getSenha().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    AbrirTelaAdministrador();
+                    Toast.makeText(MainActivity.this, "Sucesso ao Logar!", Toast.LENGTH_SHORT).show();
 
+                }else {
+                    Toast.makeText(MainActivity.this, "Usuário ou Senha inválidos! tente novamente", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
     }
+
+    private void AbrirTelaAdministrador(){
+        Intent intent = new Intent(MainActivity.this,CadastroUsuario.class);
+        startActivity(intent);
+    }
+
+    public Boolean UsuarioLogado(){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user!=null){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    public void abrirNovaActivity(Intent intent){
+        startActivity(intent);
+    }
+
 }
